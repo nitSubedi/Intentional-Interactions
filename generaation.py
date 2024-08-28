36b980d6f3657c9b74c868888a68b9efa39de2c5
nitSubedi <nsubedi@go.olemiss.edu>
2024-08-28 13:06:44 -0500

    added llm generation

---
+++ generaation.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+
+client = OpenAI(api_key="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")
+
+def geenerate_description(event_name):
+    response = client.chat.completions.create(
+        model=  "gpt-3.5-turbo",
+        messages=[
+            {"role":"system","content":"You are an Resident Assistant in a university dorm. writing your intentional interactions with your residents"},
+            {"role":"user", "content": "Write an intentional interaction with a resident about {event_name}" }       ],
+        temperature=0.7,
+    )
+    description = response.choices[0].text.strip()
+    print (description)
+
+
+geenerate_description("upcoming final exams")
